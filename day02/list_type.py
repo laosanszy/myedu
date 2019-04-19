4d8681b644d8a071f30c43d9d8c0f63514154c14
史振豫 <shizhenyu19960920@163.com>
2019-04-19 18:02:26 +0800

    list_type

--- day02/list_type.py
+++ day02/list_type.py
@@ -1,17 +1,52 @@
+alist = [1, 'shi', '我是', '谁', '123']
+
 def list_sel(a):
     print(a[4])
     print(a[-3])
     print(a[0:3])
     print(a[3:3])
     print(a[0:-1])
+def list_del():
+    alist = [1, 'shi', '我是', '谁', '123']
+    alist.pop()
+    print(alist)
+    alist.pop(2)
+    print(alist)
+    alist.pop('1')
+    print(alist)
+def list_add():
+    alist = [1, 'shi', '我是', '谁', '123']
+    alist.append('laozi')
+    print(alist)
+    blit = '123','456',[1,2,3]
+    alist.append(blit)
+    print(alist)
+    blit = alist[3:8]
+    alist.append(alist)
+    print(blit)
+    alist.pop(-1)
+    alist[0] = alist.pop(-1)
+    print(alist)
+    alist[0] = 6
+    print(alist)
+    alist[2]
+    b = 6
+    alist[2] = b
+    print(alist)
 
+def a():
+    alist[2] = 6
+    print(alist)
 
 
 
 
 if __name__ == '__main__':
-    alist = [1,'shi','我是','谁','123']
-    list_sel(alist)
+    # alist = [1,'shi','我是','谁','123']
+    # list_sel(alist)
+    # list_del()
+    # list_add()
+    a()
 
 
 
